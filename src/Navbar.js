8be06dbf6b6880cb6eb85c72e24ba97bd6bd2d4c
pawan <pawanjalandhara@gmail.com>
2024-11-11 21:53:32 +0530

    roadmap

---
+++ src/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div className="navBar">
+          <ul>
+            <li>
+              <a href="##" title="Home" target="_blank">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="##" title="About" target="_blank">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="##" title="Contact" target="_blank">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="containerAlign">
+          <h1>This is a nature of Beauty</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
