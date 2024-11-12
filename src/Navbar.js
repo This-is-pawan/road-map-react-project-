1dd942b7b0d0653afe7671f7ef75ecc5e6111738
pawan <pawanjalandhara@gmail.com>
2024-11-12 15:59:24 +0530

    popShow

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,37 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+   
+    setShowPopup(true);
+  }, []);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
     <div>
       <section>
-      <nav>
-        <div className="navBar">
-          <ul>
-            <li>
-              <a href="#home" title="Home" >
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#about" title="About">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#contact" title="Contact" >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <nav>
+          <div className="navBar">
+            <ul>
+              <li>
+                <a href="#home" title="Home">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#about" title="About">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#contact" title="Contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-      <div className="container">
-        <div className="containerAlign">
-          <h1>This is a nature of Beauty</h1>
+        <div className="container">
+          <div className="containerAlign">
+            <h1>This is a nature of Beauty</h1>
+          </div>
         </div>
-      </div>
       </section>
+
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <h2>Welcome!</h2>
+            <p>thank's for visiting <a href="#home"><b>click for premium </b></a></p>
+            <button className="btn" onClick={handleClosePopup}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
